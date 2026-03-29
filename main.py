ebb93aa96f7d8c8b3918d2b74aa549be5b807337
yashupadhyay007 <yashupadhyay2610@gmail.com>
2026-03-29 23:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-day_list= []
-price_list= []
+day_list = []
+price_list = []
 
-with open("stock_data.csv","r") as file:
+# Read stock data from CSV file
+with open("stock_data.csv", "r") as file:
     reader = csv.reader(file)
     next(reader)
 
@@ -15,40 +16,41 @@ with open("stock_data.csv","r") as file:
 change_list = []
 decision_list = []
 
+# Calculate percentage change and decision
 for i in range(len(price_list)):
     if i == 0:
         change_list.append("-")
         decision_list.append("HOLD")
     else:
-        yesterday_price= price_list[ i - 1 ]
-        today_price= price_list[ i ]
+        yesterday_price = price_list[i - 1]
+        today_price = price_list[i]
 
-        change = (( today_price - yesterday_price ) / yesterday_price ) * 100
-        change = round( change , 2 )
+        change = ((today_price - yesterday_price) / yesterday_price) * 100
+        change = round(change, 2)
 
         change_list.append(change)
 
         if change <= -2:
-            decision_list.append( "BUY" )
+            decision_list.append("BUY")
         elif change >= 2:
-            decision_list.append( "SELL" )
+            decision_list.append("SELL")
         else:
-            decision_list.append( "HOLD" )
+            decision_list.append("HOLD")
 
 profit_list = []
 have_stock = False
 buy_value = 0
 total_profit = 0
 
-buy_day_list =[]
+buy_day_list = []
 buy_price_list = []
 sell_day_list = []
 sell_price_list = []
 
 for i in range(len(day_list)):
-    today_decision = decision_list[ i ]
-    today_price = price_list[ i ]
-    today_day = day_list[ i ]
+    today_decision = decision_list[i]
+    today_price = price_list[i]
+    today_day = day_list[i]
 
     profit = 0
 
@@ -68,35 +70,38 @@ for i in range(len(day_list)):
 
     profit_list.append(profit)
 
+# Save output to CSV
 with open("output_table.csv", "w") as file:
     writer = csv.writer(file)
     writer.writerow(["Day", "Price", "Change%", "Decision", "Profit"])
 
     for i in range(len(day_list)):
         writer.writerow([
-            day_list[ i ],
-            price_list[ i ],
-            change_list[ i ],
-            decision_list[ i ],
-            profit_list[ i ]
+            day_list[i],
+            price_list[i],
+            change_list[i],
+            decision_list[i],
+            profit_list[i]
         ])
 
+# Print output in terminal
 print("Trading Result")
 print("-" * 45)
 print("Day\tPrice\tChange%\tDecision\tProfit")
 
 for i in range(len(day_list)):
     print(
-        str(day_list[ i ]) + "\t" +
-        str(price_list[ i ]) + "\t" +
-        str(change_list[ i ]) + "\t" +
-        str(decision_list[ i ]) + "\t\t" +
-        str(profit_list[ i ])
+        str(day_list[i]) + "\t" +
+        str(price_list[i]) + "\t" +
+        str(change_list[i]) + "\t" +
+        str(decision_list[i]) + "\t\t" +
+        str(profit_list[i])
     )
 
 print("\nTotal Profit:", total_profit)
 print("Output saved in output_table.csv")
 
+# Plot graph
 plt.plot(day_list, price_list, marker="o", label="Price")
 plt.scatter(buy_day_list, buy_price_list, marker="^", s=100, label="BUY")
 plt.scatter(sell_day_list, sell_price_list, marker="v", s=100, label="SELL")
